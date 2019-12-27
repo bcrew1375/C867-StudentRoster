1754315f1ce0b1bc2c002c1e8cdd7423c1445139
bcrew1375 <no-reply>
2019-12-27 17:18:50 -0500

    Project commit.

--- StudentRoster/networkStudent.cpp
+++
@@ -1,9 +0,0 @@
-#include "networkStudent.h"
-
-Degree NetworkStudent::getDegreeProgram() {
-	return degreeType;
-}
-
-void NetworkStudent::setDegreeProgram(Degree degreeProgram) {
-	degreeType = degreeProgram;
-}
